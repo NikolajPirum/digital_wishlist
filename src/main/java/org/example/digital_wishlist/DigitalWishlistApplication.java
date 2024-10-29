3d1c6b3631a66854320d171f82e96ccbd71a13e1
nikolajpirum <nikolajpirum.10@gmail.com>
2024-10-29 10:53:43 +0100

    Initial commit

---
+++ src/main/java/org/example/digital_wishlist/DigitalWishlistApplication.java
@@ -0,0 +1,13 @@
+package org.example.digital_wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalWishlistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalWishlistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/digital_wishlist/DigitalWishlistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.digital_wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalWishlistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
